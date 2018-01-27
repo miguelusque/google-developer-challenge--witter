8c25d24bb1eaa86eb6bd0f4f5eb0edcbcd8307bb
Miguel Ángel Martínez <miguelangel@users.noreply.github.com>
2018-01-27 07:48:24 +0100

    Cache elements (js, css, images, fonts...)

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,20 @@
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    // TODO: open a cache named 'wittr-static-v1'
+    // Add cache the urls from urlsToCache
+    caches.open('wittr-static-v1').then((cache) => (
+      cache.addAll([
+        '/',
+        'js/main.js',
+        'css/main.css',
+        'imgs/icon.png',
+        'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+        'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+      ])
+    ))
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     fetch(event.request)
